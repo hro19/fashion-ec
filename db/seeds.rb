c2a3a35475c60ab5b58be818e2f3c18da62d44f3
hor19 <dropmoment19@gmail.com>
2023-12-14 21:08:57 +0900

    seedファイルを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.create([
+  { name: 'Black T-shirt', description: 'この季節にカットソー。', price: '1500', image: 'https://pub-e0ab700bbdea42debc6d127861efe865.r2.dev/jules-bassoleil-_S8XcvSpQo4-unsplash.jpg' },
+  { name: 'White T-shirt', description: 'あなたにピッタリのカットソー。', price: '2300', image: 'https://pub-e0ab700bbdea42debc6d127861efe865.r2.dev/meetings-1149198_1280.jpg' },
+  { name: 'Gray T-shirt', description: 'えり好みしないカットソー。', price: '6800', image: 'https://pub-e0ab700bbdea42debc6d127861efe865.r2.dev/money-2724241_1280.jpg' },
+])
