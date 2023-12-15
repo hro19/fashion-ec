520609d203c3d3c0fdf51b41a8fb157e8bbd1cd4
hor19 <dropmoment19@gmail.com>
2023-12-15 18:48:01 +0900

    初回訪問にsession情報を登録する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :create_cart
+ 
+  private
+ 
+    def create_cart
+      if !session[:cart_id]
+        cart = Cart.create
+        session[:cart_id] = cart.id
+      end
+    end
 end
