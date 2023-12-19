2300e485c790274a36faaa2490d2829fc9168e6c
hro19 <fourbasic19@gmail.com>
2023-12-20 01:56:43 +0900

    Rubyの標準ライブラリである dateを使って日付を調整

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 module ApplicationHelper
-    include ActiveSupport::NumberHelper
+  include ActiveSupport::NumberHelper
+
+  require "date"
+  def updated_at_jp(date)
+    date.strftime("%Y年%m月%d日") 
+  end
 end
