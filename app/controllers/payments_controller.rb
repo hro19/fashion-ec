876e908030bbb202d27a2a450f79b40da8e5b021
hro19 <fourbasic19@gmail.com>
2023-12-21 20:45:00 +0900

    stripeCheckoutのclassを作成

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,30 +1,15 @@
 class PaymentsController < ApplicationController
   before_action :set_cart, only: %i[checkout success]
  
+  # Checkout action handles the payment process for the cart.
+  # If the cart is empty, it redirects to the carts page.
+  # It creates a Stripe checkout session based on the line items in the cart.
+  # The session is used to initiate the payment process.
+  #
+  # @since Rails 5
   def checkout
-    line_items = []
- 
-    @cart.line_items.each do |line_item|
-      line_items << {
-        price_data: {
-          unit_amount: line_item.product.price,
-          currency: 'jpy',
-          product_data: {
-            name: line_item.product.name,
-            description: line_item.product.description,
-          }
-        },
-        quantity: line_item.quantity,
-      }
-    end
- 
-    @session = Stripe::Checkout::Session.create({
-      payment_method_types: ['card'],
-      line_items: line_items,
-      mode: 'payment',
-      success_url: payments_success_url,
-      cancel_url: carts_url,
-    })
+    redirect_to carts_path and return if @cart.line_items.empty?
+    @session = Stripe::Checkout::Session.create(StripeCheckout.make_stripe_checkout_params(cart: @cart))
   end
 
   def success
