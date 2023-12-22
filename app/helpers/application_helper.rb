4215bab98674bf56ec21b81015e05e950f16f104
hro19 <fourbasic19@gmail.com>
2023-12-22 15:49:20 +0900

    税込み価格のヘルパーを作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,14 @@ module ApplicationHelper
   def updated_at_jp(date)
     date.strftime("%Y年%m月%d日") 
   end
+
+  def include_tax(price)
+    taxed_price = BigDecimal(price * 1.1, 2).floor # 精度を2桁に指定
+  end
+
+  def include_tax_in_yen(price)
+    taxed_price = include_tax(price)
+    return number_to_delimited(taxed_price)
+  end
+
 end
