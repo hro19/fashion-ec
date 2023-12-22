3674b30a46e9d6e0a8427d672874b7863fe40739
hro19 <fourbasic19@gmail.com>
2023-12-22 15:59:00 +0900

    バグフィックス、精度を4桁にする

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,11 @@ module ApplicationHelper
 
   require "date"
   def updated_at_jp(date)
-    date.strftime("%Y年%m月%d日") 
+    date.strftime("%Y年%m月%d日")
   end
 
   def include_tax(price)
-    taxed_price = BigDecimal(price * 1.1, 2).floor # 精度を2桁に指定
+    taxed_price = BigDecimal(price * 1.1, 4).floor # 精度を4桁に指定
   end
 
   def include_tax_in_yen(price)
